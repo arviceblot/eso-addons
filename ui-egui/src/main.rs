6c566ef355ec99ceaab0c1aebcd5635b1aea0ea4
Logan Sales <logan@arviceblot.com>
2023-02-21 21:05:30 -0600

    Add more UI functionality

--- ui-egui/src/main.rs
+++ ui-egui/src/main.rs
@@ -1,16 +1,57 @@
 use eframe::egui::{self, ScrollArea};
-use eso_addons_core::service::result::AddonDetails;
+use eso_addons_core::service::result::SearchDbAddon;
 use eso_addons_core::service::AddonService;
+use std::fmt;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 use tokio::runtime;
 
 const APP_NAME: &str = "ESO Addon Manager";
+const REPO: Option<&str> = option_env!("CARGO_PKG_REPOSITORY");
+
+#[derive(Debug, PartialEq, Clone, Copy, EnumIter)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+enum Sort {
+    Name,
+    Updated,
+    Author,
+    TotalDownloads,
+    MonthlyDownloads,
+    Favorites,
+    Id,
+}
+impl fmt::Display for Sort {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Sort::Name => write!(f, "Name"),
+            Sort::Updated => write!(f, "Updated"),
+            Sort::Author => write!(f, "Author"),
+            Sort::TotalDownloads => write!(f, "Total Downloads"),
+            Sort::MonthlyDownloads => write!(f, "Monthly Downloads"),
+            Sort::Favorites => write!(f, "Favorites"),
+            Sort::Id => write!(f, "ID"),
+        }
+    }
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum View {
+    Installed,
+    Search,
+    Browse,
+}
 
 struct EamApp {
     rt: runtime::Runtime,
     service: AddonService,
     init: bool,
-    installed_count: i32,
     addons_updated: Vec<String>,
+    installed_addons: Vec<SearchDbAddon>,
+    filter: String,
+    sort: Sort,
+    prev_sort: Sort,
+    view: View,
 }
 
 impl EamApp {
@@ -24,9 +65,13 @@ impl EamApp {
         EamApp {
             rt,
             init: true,
-            installed_count: 0,
             service,
             addons_updated: vec![],
+            installed_addons: vec![],
+            filter: Default::default(),
+            sort: Sort::Name,
+            prev_sort: Sort::Name,
+            view: View::Installed,
         }
     }
 
@@ -36,12 +81,6 @@ impl EamApp {
             self.init = false;
         }
         init
-    }
-    fn get_installed_addon_count(&mut self) {
-        self.installed_count = self
-            .rt
-            .block_on(self.service.get_installed_addon_count())
-            .unwrap();
     }
     fn update_addons(&mut self) {
         let result = self.rt.block_on(self.service.update()).unwrap();
@@ -54,32 +93,135 @@ impl EamApp {
                 .push("Everything up to date!".to_string());
         }
     }
+    fn get_installed_addons(&mut self) {
+        let result = self
+            .rt
+            .block_on(self.service.get_installed_addons())
+            .unwrap();
+        self.installed_addons = result;
+        self.sort_addons();
+    }
+    fn handle_sort(&mut self) {
+        if self.prev_sort != self.sort {
+            self.prev_sort = self.sort;
+            self.sort_addons();
+        }
+    }
+    fn sort_addons(&mut self) {
+        match self.sort {
+            Sort::Author => {
+                // TODO:
+            }
+            Sort::Name => self
+                .installed_addons
+                .sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase())),
+            Sort::Updated => {
+                // TODO: add date to SearchDbAddon to use this sort
+            }
+            Sort::TotalDownloads => {
+                // TODO:
+            }
+            Sort::MonthlyDownloads => {
+                // TODO:
+            }
+            Sort::Favorites => {
+                // TODO:
+            }
+            Sort::Id => self.installed_addons.sort_by(|a, b| a.id.cmp(&b.id)),
+        }
+    }
 }
 
 impl eframe::App for EamApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if self.show_init() {
                 // TODO: move blocking install count out of update loop!
-                self.get_installed_addon_count();
+                self.get_installed_addons();
             }
-            ui.horizontal(|ui| {
-                ui.label(format!("Installed: {}", self.installed_count));
+            self.handle_sort();
+
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Save").clicked() {
+                        // TODO: Add functionality
+                    }
+                    if ui.button("Quit").clicked() {
+                        frame.close();
+                    }
+                });
+                ui.menu_button("Help", |ui| {
+                    if ui.button("Logs").clicked() {
+                        // TODO: Add functionality
+                    }
+                    if ui.button("About").clicked() {
+                        // TODO: Add functionality
+                    }
+                    if REPO.is_some() {
+                        ui.hyperlink_to("Source on GitHub", REPO.unwrap());
+                    }
+                })
             });
-            if ui.button("Update").clicked() {
-                // TODO: move blocking update out of update loop!
-                self.update_addons();
+            ui.separator();
+            ui.horizontal(|ui| {
+                ui.selectable_value(&mut self.view, View::Installed, "Installed");
+                ui.selectable_value(&mut self.view, View::Search, "Search");
+                ui.selectable_value(&mut self.view, View::Browse, "Browse");
+            });
+            ui.separator();
+
+            if self.installed_addons.is_empty() {
+                ui.label("No addons installed!");
+            } else {
+                if ui.button("Update").clicked() {
+                    // TODO: move blocking update out of update loop!
+                    self.update_addons();
+                }
+                ui.label(format!("Installed: {}", self.installed_addons.len()));
+                ui.horizontal(|ui| {
+                    ui.label("Filter:");
+                    ui.add(egui::TextEdit::singleline(&mut self.filter).desired_width(120.0));
+                    self.filter = self.filter.to_lowercase();
+                    if ui.button("ｘ").clicked() {
+                        self.filter.clear();
+                    }
+                    egui::ComboBox::from_label("Sort")
+                        .selected_text(format!("{}", self.sort))
+                        .show_ui(ui, |ui| {
+                            ui.style_mut().wrap = Some(false);
+                            ui.set_min_width(60.0);
+                            for sort in Sort::iter() {
+                                ui.selectable_value(&mut self.sort, sort, sort.to_string());
+                            }
+                        });
+                });
+                ui.separator();
+                ui.vertical_centered_justified(|ui| {
+                    ScrollArea::vertical()
+                        .max_height(200.0)
+                        .auto_shrink([false; 2])
+                        .show(ui, |ui| {
+                            ui.vertical(|ui| {
+                                for addon in self.installed_addons.iter() {
+                                    ui.label(addon.name.as_str());
+                                }
+                            });
+                        });
+                });
+                ui.separator();
             }
 
-            // update log scroll area
-            let scroll_area = ScrollArea::vertical()
-                .max_height(200.0)
-                .auto_shrink([false; 2]);
-            scroll_area.show(ui, |ui| {
-                ui.vertical(|ui| {
-                    for update in self.addons_updated.iter() {
-                        ui.label(update);
-                    }
+            // log scroll area
+            ui.collapsing("Log", |ui| {
+                ui.horizontal_wrapped(|ui| {
+                    ui.spacing_mut().item_spacing.x = 0.0;
+                    ScrollArea::vertical().max_height(20.0).show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            for update in self.addons_updated.iter() {
+                                ui.label(update);
+                            }
+                        });
+                    });
                 });
             });
         });
